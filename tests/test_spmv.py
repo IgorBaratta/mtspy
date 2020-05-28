f1e2a765f74aced118f0b9cbb3122e16c90f7c82
igorbaratta <igorbaratta@gmail.com>
2020-05-28 01:32:59 +0000

    increase test coverage

--- tests/test_spmv.py
+++ tests/test_spmv.py
@@ -1,22 +1,34 @@
 import numpy
 from scipy import sparse
+import pytest
 import mtspy_cpp
 
 
 def mat_vec(A, b):
     rows, cols = A.shape
     nnz = A.nnz
-    x = mtspy_cpp.mat_vec_d(rows, cols, nnz,
-                            A.data, A.indptr,
-                            A.indices, b)
+    dtype = b.dtype
+
+    if dtype == numpy.float64:
+        op = mtspy_cpp.mat_vec_d
+    elif dtype == numpy.float32:
+        op = mtspy_cpp.mat_vec_f
+    elif dtype == numpy.complex64:
+        op = mtspy_cpp.mat_vec_cf
+    elif dtype == numpy.complex128:
+        op = mtspy_cpp.mat_vec_cd
+
+    x = op(rows, cols, nnz,
+           A.data, A.indptr,
+           A.indices, b)
     return x
 
 
-N = 1000
-v0 = numpy.ones(N, dtype=numpy.float64)
-M = sparse.random(N, N, density=0.1, format="csr", dtype=numpy.float64)
-
-v1 = mat_vec(M, v0)
-v2 = M@v0
-
-assert(numpy.allclose(v1, v2))
+@pytest.mark.parametrize('dtype', [numpy.float32, numpy.float64, numpy.complex64, numpy.complex128])
+def test_spmv(dtype):
+    N = 1000
+    v0 = numpy.ones(N, dtype=dtype)
+    M = sparse.random(N, N, density=0.1, format="csr", dtype=dtype)
+    v1 = mat_vec(M, v0)
+    v2 = M@v0
+    assert(numpy.allclose(v1, v2))
