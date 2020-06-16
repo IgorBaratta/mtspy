93fe116de8785d19bf66510111d70c9f4dffa105
IgorBaratta <igorbaratta@gmail.com>
2020-06-16 19:18:46 -0300

    add eigen 64 bits

--- tests/test_spmv.py
+++ tests/test_spmv.py
@@ -36,13 +36,14 @@ def test_spmm(dtype, use_eigen):
 
 
 @pytest.mark.parametrize('dtype', dtype_list)
-def test_spmv_64bit_ind(dtype):
+@pytest.mark.parametrize('use_eigen', eigen_backend)
+def test_spmv_64bit_ind(dtype, use_eigen):
     N = 1000
     v0 = numpy.random.rand(N, 1).astype(dtype)
     M = sparse.random(N, N, density=0.1, format="csr", dtype=dtype)
     # Change sparse matrix indices dtype
     M.indices = M.indices.astype(numpy.int64)
     M.indptr = M.indptr.astype(numpy.int64)
-    v1 = mtspy.matvec(M, v0)
+    v1 = mtspy.matvec(M, v0, use_eigen)
     v2 = M @ v0
     assert(numpy.allclose(v1, v2))
